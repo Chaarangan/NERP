b43f316f987d86acf9b0cddf2d359a0e37a85578
Chaarangan <charangan.18@cse.mrt.ac.lk>
2022-05-07 15:18:29 +0530

    setup finished

---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="NERP",
+    version="1.0.0",
+    author="Charangan Vasantharajan",
+    author_email="charangan.18@cse.mrt.ac.lk",
+    description="A Pipeline for Finetuning & Infering Transformers for Named-Entity Recognition",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chaarangan/NERP",
+    packages=setuptools.find_packages(where='src'),
+    package_dir={'': 'src'},
+    python_requires='>=3.6',
+    install_requires=[
+        'torch',
+        'transformers',
+        'sklearn',
+        'nltk',
+        'sentencepiece'
+    ],
+    # setup_requires=['pytest-runner'],
+    # tests_require=['pytest',
+    #                'pytest-cov'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    include_package_data=True,
+    dependency_links=['https://github.com/Chaarangan/NERDA/tree/save_and_load_tokenizer']
+)
---
+++ src/NERP/prepare_data.py
@@ -0,0 +1,40 @@
+import os
+import pandas as pd
+
+from NERP.utils import SentenceGetter
+
+
+def prepare_data(limit: int = None,
+                 file_path: str = None
+                 ) -> dict:
+    """
+    Args:
+        limit (int, optional): Limit the number of observations to be 
+            returned from a given split. Defaults to None, which implies 
+            that the entire data split is returned.
+        file_path (str, required): file where data is cached.
+
+    Returns:
+        list of sentences, and entities
+    """
+    file_path = os.path.join(file_path)
+    assert os.path.isfile(file_path), f'File {file_path} does not exist.'
+
+    data = pd.read_csv(file_path)
+    data = data.fillna(method="ffill")
+
+    getter = SentenceGetter(data)
+    sentences = [[word[0] for word in sentence]
+                 for sentence in getter.sentences]
+    entities = [[s[1] for s in sentence] for sentence in getter.sentences]
+
+    if limit != 0:
+        assert isinstance(limit, int), f"Limit shoud be a int!"
+        sentences = sentences[:limit]
+        entities = entities[:limit]
+        print("Dataset is limited to {}".format(limit))
+
+    assert len(sentences) == len(
+        entities), f"Sentences and entities are having different length."
+
+    return {'sentences': sentences, 'tags': entities}
